76084312f650d92389f25f7c87c490a69f51d8bc
nesmamanasra <nesmazak1999@gmail.com>
2026-01-20 12:51:38 +0200

    delete app file amd update routes file

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,15 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import PublicLayout from "./layouts/PublicLayout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import { isLoggedIn } from "./lib/auth";
 
 export const router = createBrowserRouter([
   {
     element: <PublicLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/login", element: <Login /> },
+      {
+        path: "/login",
+        element: isLoggedIn() ? <Navigate to="/" replace /> : <Login />,
+      },
       { path: "*", element: <NotFound /> },
     ],
   },
